de5926573217e8663f669b82016d7195c206adae
陈后根 <chenhougen@corp.netease.com>
2022-07-01 16:35:48 +0800

    add nasl change record intercepter

--- src/main/java/com/netease/cloud/lowcode/naslstorage/config/WebConfig.java
+++ src/main/java/com/netease/cloud/lowcode/naslstorage/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.netease.cloud.lowcode.naslstorage.config;
 
 import com.netease.cloud.lowcode.naslstorage.interceptor.AppIdInterceptor;
+import com.netease.cloud.lowcode.naslstorage.interceptor.NaslChangeRecordInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AppIdInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns( "/health");
+
+        registry.addInterceptor(new NaslChangeRecordInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns( "/health");
     }
 }
--- src/main/java/com/netease/cloud/lowcode/naslstorage/interceptor/NaslChangeRecordInterceptor.java
+++ src/main/java/com/netease/cloud/lowcode/naslstorage/interceptor/NaslChangeRecordInterceptor.java
@@ -1,7 +1,5 @@
 package com.netease.cloud.lowcode.naslstorage.interceptor;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,36 +8,31 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author pingerchen
- * 应用id 信息统一由header 传入
+ * 记录前端Nasl 更改或后端Nasl 更改的时间，用于发布时判断是否需要重新生成代码和cicd
  */
-@Slf4j
-public class AppIdInterceptor implements HandlerInterceptor {
-
-    private static final String APP_ID = "appId";
+public class NaslChangeRecordInterceptor implements HandlerInterceptor {
+    private static final String NASL_TYPE = "NASLType";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
-        String appId = request.getHeader(APP_ID);
-        if (!StringUtils.hasLength(appId)) {
-            log.error("appId in header is required");
-            throw new Exception("appId in header is required");
-        }
-        AppIdContext.set(appId);
+        /**
+         * 理论上Nasl 操作成功后在更新Nasl 更改时间的，但切面和具体Nasl 操作的逻辑不好整合在一个事务里
+         * 更新前先设置Nasl 更新时间影响较小，挺多就是Nasl 没变更成功，但是还是重新生成代码，没功能问题，体验不好
+         */
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
+                           ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
-        AppIdContext.clear();
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
